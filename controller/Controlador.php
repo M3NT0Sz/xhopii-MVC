d1e35d28b330de934e0604b7e4b8b4e4ba6862a9
M3NT0Sz <matheusmendes2005@outlook.com>
2024-08-28 13:12:53 -0300

    xhopiiMVC

---
+++ controller/Controlador.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once "../model/BancoDeDados.php";
+require_once "../model/Produto.php";
+
+class Controlador
+{
+    //Atributos
+    private $bancoDeDados;
+
+    public function __construct()
+    {
+        $this->bancoDeDados = new BancoDeDados("localhost", "root", "", "xhopii");
+    }
+
+    // public function __construct($host, $login, $senha, $dataBase){
+    //     $this->bancoDeDados = new BancoDeDados("$host","$login", "$senha", "$dataBase");
+    // }
+
+    public function cadastrarProduto($nome, $fabricante, $descricao, $valor){
+        $produto = new Produto($nome, $fabricante, $descricao, $valor);
+        $this->bancoDeDados->inserirProduto($produto);
+    }
+
+    public function visualizarProduto(){
+        $listaProdutos = $this->bancoDeDados->retornarProdutos();
+        while($produto = mysqli_fetch_assoc($listaProdutos)){
+            return "<section class=\"conteudo-bloco\">".
+            "<h2>" . $produto["nome"] . "</h2>" .
+            "<p>Fabricante: " . $produto["fabricante"] . "</p>" .
+            "<p>Descrição: " . $produto["descricao"] . "</p>" .
+            "<p>Valor: " . $produto["valor"] . "</p>" .
+            "</section>";
+        }
+    }
+}
